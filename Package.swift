b95565fe7a95cfa815ea28538a89eb995563ed70
Aruna Kumari Yarra <aruna.yarra@broadcom.com>
2023-09-14 16:15:26 +0530

    no message

--- Package.swift
+++ Package.swift
@@ -12,8 +12,6 @@ let package = Package(
             targets: ["SampleSPM"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/CA-Application-Performance-Management/CAMobileAppAnalytics.git", branch: "spm")
     ],
     targets: [
